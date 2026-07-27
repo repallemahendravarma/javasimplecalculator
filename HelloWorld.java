2b088f2d757abfd6380ca3e8425dfba17d45407a
repallemahendravarma <mahedravarma23@lpu.in>
2026-07-27 10:21:51 +0530

    simple calculator

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World dfghjkjhgfdsa!");
+        System.out.println("Hello, Kya haal chall h jenkins Poll Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm");
+        System.out.println("Hello, Kya haal chall h jenkins Webhook Scm one more");
+        System.out.println("Hello, this is demo 1 of poll scm");
+        System.out.println("Hello, this is demo 2 of poll scm");
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+this is a java simple calculator by using Switch case 
+
+
